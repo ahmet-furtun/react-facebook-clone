32ef827cdd8f9655a8578f6b631cab4cc772c1a5
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-23 12:33:08 +0300

    Firebase config put and Navbar design is finalized

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from '../header/header.component';
+
 import '../../common/styles';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAubCsfJlk_3oZBONRrZfGK83Bfi8iJzvU",
+    authDomain: "facebook-clone-aea79.firebaseapp.com",
+    projectId: "facebook-clone-aea79",
+    storageBucket: "facebook-clone-aea79.appspot.com",
+    messagingSenderId: "938475679750",
+    appId: "1:938475679750:web:5a3851848796c6fc211387",
+    measurementId: "G-QK67P9ST0C"
+  };
